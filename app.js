69ab0dec757273ff9a36e3c28924f7fc62868f63
Roland Osaro Aifuwa <siroapple10@gmail.com>
2020-09-30 01:37:47 -0500

    post request

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express");
-const https = require("https");
 const bodyParser= require("body-parser");
+
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 res.sendFile(__dirname+"/signup.html");
 });
 
-app.listen(3000, function () {
+app.post("/",function(req, res){
+    console.log("Post received");
+
+var firstname=req.body.f1;
+var secname= req.body.f2;
+var email=req.body.email;
+
+
+
+});
+app.listen(3000, function (req, res) {
     
     console.log("server is running on port 3000");
 });
