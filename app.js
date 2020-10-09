5145aa4805785f6a5e81add315f79193f089d723
Roland Osaro Aifuwa <siroapple10@gmail.com>
2020-10-09 01:24:09 -0500

    post

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
 const express = require("express");
-const bodyParser= require("body-parser");
-
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
-res.sendFile(__dirname+"/signup.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/",function(req, res){
+app.post("/", function (req, res) {
     console.log("Post received");
 
-var firstname=req.body.f1;
-var secname= req.body.f2;
-var email=req.body.email;
+    const firstname = req.body.f1;
+    const secname = req.body.f2;
+    const email = req.body.email;
+
+    console.log(firstname, secname, email);
 
 
+    const data = {
+        members: [
+            {
+                email_address: email,
+            status: "subscribed",
+                merge_feilds: {
+                    FNAME: firstname,
+                    LNAME: secname
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data);
+    const url = "https://us2.api.mailchimp.com/3.0/lists/80c3843d52.";
+    const options={
+        method: "POST",
+        auth: "roland1:acf7cd3c3babfa6a474d341e211ce60c-us2"
+    }
+    const request=https.request(url, options, function (response) {
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+request.write(jsonData);
+request.end();
 
 });
 app.listen(3000, function (req, res) {
-    
+
     console.log("server is running on port 3000");
 });
+
+//app key
+//acf7cd3c3babfa6a474d341e211ce60c-us2
+//unique id:
+// 80c3843d52.
