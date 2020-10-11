045115c09d430f3481a6dd5ee2fbff2a651e7066
Roland O. Aifuwa <siroapple10@gmail.com>
2020-10-10 20:46:29 -0500

    added mailchimp api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const https= require("https");
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,49 +10,60 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
+
+
+
 app.post("/", function (req, res) {
-    console.log("Post received");
-
-    const firstname = req.body.f1;
-    const secname = req.body.f2;
+    const firstName = req.body.f1;
+    const lastName = req.body.f2;
     const email = req.body.email;
-
-    console.log(firstname, secname, email);
-
-
+   
     const data = {
-        members: [
-            {
-                email_address: email,
-            status: "subscribed",
-                merge_feilds: {
-                    FNAME: firstname,
-                    LNAME: secname
-                }
-            }
-        ]
+      members: [
+        {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+          }
+        }
+      ]
     }
-    const jsonData = JSON.stringify(data);
-    const url = "https://us2.api.mailchimp.com/3.0/lists/80c3843d52.";
-    const options={
-        method: "POST",
-        auth: "roland1:acf7cd3c3babfa6a474d341e211ce60c-us2"
+   
+    const postData = JSON.stringify(data);
+   
+    const options = {
+      url: "https://us2.api.mailchimp.com/3.0/lists/80c3843d52",
+      method: "POST",
+      headers: {
+        Authorization: "auth acf7cd3c3babfa6a474d341e211ce60c-us2"
+      },
+      body: postData
     }
-    const request=https.request(url, options, function (response) {
-        response.on("data", function(data){
-            console.log(JSON.parse(data));
-        });
+   
+    request(options, function (err, response, body) {
+      if(err){
+        console.log("Uh oh, there was some kind of Error!");
+      } else {
+        if (response.statusCode === 200) {
+          console.log("Success");
+           console.log(JSON.stringify(response.body));
+        } else {
+          console.log("Uh oh, there was some kind of Error!");
+        }   
+      }
     });
-request.write(jsonData);
-request.end();
-
-});
+  });
 app.listen(3000, function (req, res) {
 
     console.log("server is running on port 3000");
 });
 
+
+
 //app key
 //acf7cd3c3babfa6a474d341e211ce60c-us2
 //unique id:
 // 80c3843d52.
+
